1305a9691ff7d241f7d59e21c6574955657a5942
ranahany <ranahanymohamed265@gmailcom>
2025-01-07 21:13:54 +0200

    server

---
+++ project-folder/server.js
@@ -0,0 +1,12 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/get-homepage-url', (req, res) => {
+//     res.json({ url: '/homepage.html' });
+// });
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
