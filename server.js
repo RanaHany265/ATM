d1bcbd88415c99a259329c97919e350b834f0409
ranahany <ranahanymohamed265@gmailcom>
2025-01-07 14:16:37 +0200

    edit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require (`express`);
 const path = require (`path`);
 const app = express ()
 const PORT = 3000;
-app.use(express .static (path.join(__dirname)));
+app.use(express .static (path.join(__dirname )));
 app.get('/' , (req, res)=> {
     res.sendfile(path.join(__dirname , 'index.html'));
 })
